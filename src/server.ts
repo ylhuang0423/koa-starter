662d6f9bf87259e08681396781509200e14fa35d
ylhuang <ylhuang0423@gmail.com>
2018-04-16 18:25:37 +0800

    update

--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,22 @@ import { createConnection } from "typeorm"
 import * as Koa from 'koa'
 import * as Router from 'koa-router'
 import * as bodyParser from 'koa-bodyparser'
+import * as serve from 'koa-static'
+import * as history from 'koa2-history-api-fallback'
 import routes from './routes'
 
 createConnection().then(async connection => {
   const app = new Koa()
   const router = new Router()
 
-  routes.forEach(route => {
-    router[route.method]('/api'+route.path, route.action)
-  })
+  app.use(history())
+  app.use(serve('public'))
 
   app.use(bodyParser())
   app.use(router.routes())
   app.use(router.allowedMethods())
+
+  routes.forEach(route => router[route.method]('/api'+route.path, route.action))
+
   app.listen(3000)
 }).catch(err => console.error(err))
